3d9ee0208525153a6b73226c887c26ff47cee235
yrnThiago <thiagodias_123@hotmail.com>
2023-10-31 12:03:25 -0300

    fix: change route url to '/'

--- app/views/o_que_e_scrum.py
+++ app/views/o_que_e_scrum.py
@@ -3,6 +3,6 @@ from flask import Blueprint, render_template
 o_que_e_scrum_blueprint = Blueprint('oqueescrum', __name__)
 
 
-@o_que_e_scrum_blueprint.route('/oqueescrum')
+@o_que_e_scrum_blueprint.route('/')
 def index():
     return render_template("OQueEScrum.html")
